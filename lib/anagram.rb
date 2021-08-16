9c9163f17269db1c00dd3bd9284de1237dd4f64d
Christine Contreras <christineec23@yahoo.com>
2021-08-16 16:23:56 -0500

    working

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram 
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        array.select do |w|
+            @word.chars.sort == w.chars.sort
+        end
+    end
+end
